80f826db2fe2c30c152fe6b956caed4764f831a0
igorbrehm <igorbrehm@hotmail.com>
2019-05-28 15:27:39 -0300

    primeiro commit

---
+++ atividadeKMEANS.java
@@ -0,0 +1,9 @@
+import java.utils.Scanner;
+
+public class atividadeKMEANS {
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+        int k = Integer.parseInt(args[0]);
+        
+    }
+}
